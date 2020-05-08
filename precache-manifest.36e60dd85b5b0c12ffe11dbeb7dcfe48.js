9c735838fe8c15917a35799837c3a03b5a8ad7bf
heamabc <takhei611@gmail.com>
2020-05-08 11:04:19 +0800

    Updates

---
+++ precache-manifest.36e60dd85b5b0c12ffe11dbeb7dcfe48.js
@@ -0,0 +1,426 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd998f710a3d12d7923e1c456319ddd2",
+    "url": "/fergus-quant/index.html"
+  },
+  {
+    "revision": "a0395ec00f521d9ffe4d",
+    "url": "/fergus-quant/static/css/main.3480ba0d.chunk.css"
+  },
+  {
+    "revision": "98a197d27e03d8c8e8a8",
+    "url": "/fergus-quant/static/js/2.516b06bc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/fergus-quant/static/js/2.516b06bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0395ec00f521d9ffe4d",
+    "url": "/fergus-quant/static/js/main.b84f7024.chunk.js"
+  },
+  {
+    "revision": "d57cfbc657ca3efca624",
+    "url": "/fergus-quant/static/js/runtime-main.e556baa0.js"
+  },
+  {
+    "revision": "2b7713ad26aa71bc156396d8b43bc12a",
+    "url": "/fergus-quant/static/media/Illustration1.2b7713ad.png"
+  },
+  {
+    "revision": "4b59cacb0449bdd77af2f2793b495696",
+    "url": "/fergus-quant/static/media/Illustration1.4b59cacb.png"
+  },
+  {
+    "revision": "5ea54e0fce33cb535290c372e9e18e8d",
+    "url": "/fergus-quant/static/media/Illustration1.5ea54e0f.png"
+  },
+  {
+    "revision": "d88d3a6241cb8d8f9aa7883193d757d3",
+    "url": "/fergus-quant/static/media/Illustration1.d88d3a62.png"
+  },
+  {
+    "revision": "dbb925b8a6ccf19d4e3e5e916c9b03fe",
+    "url": "/fergus-quant/static/media/Illustration2.dbb925b8.png"
+  },
+  {
+    "revision": "ada9c5027c9f81204c272c5ea1d0cd62",
+    "url": "/fergus-quant/static/media/Illustration3.ada9c502.png"
+  },
+  {
+    "revision": "6a36356092d3fd3c91d20d93817fb776",
+    "url": "/fergus-quant/static/media/LemonMilk.6a363560.ttf"
+  },
+  {
+    "revision": "6e0c2ea1fb9ad972d86ac147d72a2c83",
+    "url": "/fergus-quant/static/media/LemonMilk.6e0c2ea1.eot"
+  },
+  {
+    "revision": "81eff64a93bb3ddca896d0f1edbf4970",
+    "url": "/fergus-quant/static/media/LemonMilk.81eff64a.woff2"
+  },
+  {
+    "revision": "ad676a2b827dce316954123d6f5e1d70",
+    "url": "/fergus-quant/static/media/LemonMilk.ad676a2b.woff"
+  },
+  {
+    "revision": "2253f54734895895aa6b687ac8921a6b",
+    "url": "/fergus-quant/static/media/LemonMilkbold.2253f547.eot"
+  },
+  {
+    "revision": "240c169c68f83d34512c67208d5dac25",
+    "url": "/fergus-quant/static/media/LemonMilkbold.240c169c.woff2"
+  },
+  {
+    "revision": "519b3b25762c1945baf102117ef4bac6",
+    "url": "/fergus-quant/static/media/LemonMilkbold.519b3b25.woff"
+  },
+  {
+    "revision": "5d79e4d3dbef246c0590461bc747109c",
+    "url": "/fergus-quant/static/media/LemonMilkbold.5d79e4d3.ttf"
+  },
+  {
+    "revision": "58955d38d48b0641359a3c7d1fdede45",
+    "url": "/fergus-quant/static/media/LemonMilkbolditalic.58955d38.woff2"
+  },
+  {
+    "revision": "9e5d2bd18a13ffc443465fe225c3ec65",
+    "url": "/fergus-quant/static/media/LemonMilkbolditalic.9e5d2bd1.ttf"
+  },
+  {
+    "revision": "b09f0ea42eb45eeda83d848ea630e2f5",
+    "url": "/fergus-quant/static/media/LemonMilkbolditalic.b09f0ea4.eot"
+  },
+  {
+    "revision": "bf9db87f70aebcd3f09a210436ad2c75",
+    "url": "/fergus-quant/static/media/LemonMilkbolditalic.bf9db87f.woff"
+  },
+  {
+    "revision": "29892136913054fc79f2181afc24db2f",
+    "url": "/fergus-quant/static/media/LemonMilkitalic.29892136.eot"
+  },
+  {
+    "revision": "31f1aa02c4ad259f0c152584588b0b78",
+    "url": "/fergus-quant/static/media/LemonMilkitalic.31f1aa02.woff2"
+  },
+  {
+    "revision": "4167f2b1c2f222a27326f46311720099",
+    "url": "/fergus-quant/static/media/LemonMilkitalic.4167f2b1.ttf"
+  },
+  {
+    "revision": "74554ee7f85a0e43a06a3a5566816363",
+    "url": "/fergus-quant/static/media/LemonMilkitalic.74554ee7.woff"
+  },
+  {
+    "revision": "27646b59158e7f1dc82f02e1fcd29568",
+    "url": "/fergus-quant/static/media/LemonMilklight.27646b59.ttf"
+  },
+  {
+    "revision": "68cf858a04228a7835dc73276ae33c44",
+    "url": "/fergus-quant/static/media/LemonMilklight.68cf858a.eot"
+  },
+  {
+    "revision": "acdabdb59dc030f2ac02b0b28b9f99c6",
+    "url": "/fergus-quant/static/media/LemonMilklight.acdabdb5.woff2"
+  },
+  {
+    "revision": "b9e1f5cd6ac7d6adcd4513102da57c5c",
+    "url": "/fergus-quant/static/media/LemonMilklight.b9e1f5cd.woff"
+  },
+  {
+    "revision": "5ec13e70b0b3ab8c953aebd7ba95a5a4",
+    "url": "/fergus-quant/static/media/LemonMilklightitalic.5ec13e70.woff2"
+  },
+  {
+    "revision": "7cb58471a8473ebae9f6faef2e95e90a",
+    "url": "/fergus-quant/static/media/LemonMilklightitalic.7cb58471.ttf"
+  },
+  {
+    "revision": "846a076f2583773af3d9b43b331a3b5f",
+    "url": "/fergus-quant/static/media/LemonMilklightitalic.846a076f.eot"
+  },
+  {
+    "revision": "b0f62fcd9030e10d0d911c57b0b175eb",
+    "url": "/fergus-quant/static/media/LemonMilklightitalic.b0f62fcd.woff"
+  },
+  {
+    "revision": "13fce51322543307377414627e953dc0",
+    "url": "/fergus-quant/static/media/Resume Kwan_Tak_Hei_Fergus.13fce513.pdf"
+  },
+  {
+    "revision": "1be05e5f249e7e50d0872510c63943b0",
+    "url": "/fergus-quant/static/media/Roboto-Black.1be05e5f.woff2"
+  },
+  {
+    "revision": "29b227da7864fd272f9fe321c916732a",
+    "url": "/fergus-quant/static/media/Roboto-Black.29b227da.woff"
+  },
+  {
+    "revision": "d521e6419140e849e1864b3efd7cc1fe",
+    "url": "/fergus-quant/static/media/Roboto-Black.d521e641.ttf"
+  },
+  {
+    "revision": "eb76b1507e7850d087b5e6fbc9217606",
+    "url": "/fergus-quant/static/media/Roboto-Black.eb76b150.eot"
+  },
+  {
+    "revision": "177faf115b488af24064bd864afa8a18",
+    "url": "/fergus-quant/static/media/Roboto-BlackItalic.177faf11.woff2"
+  },
+  {
+    "revision": "2742ffeadd1d52305727d2532f74a22b",
+    "url": "/fergus-quant/static/media/Roboto-BlackItalic.2742ffea.ttf"
+  },
+  {
+    "revision": "95fc6a9fb482b78a19d268a002b02513",
+    "url": "/fergus-quant/static/media/Roboto-BlackItalic.95fc6a9f.eot"
+  },
+  {
+    "revision": "fa507b7bdd6b187a14d7bce6b4bff079",
+    "url": "/fergus-quant/static/media/Roboto-BlackItalic.fa507b7b.woff"
+  },
+  {
+    "revision": "067d2055e898f095bbb0307c08362aa5",
+    "url": "/fergus-quant/static/media/Roboto-Bold.067d2055.woff"
+  },
+  {
+    "revision": "761d6d509fc2e365520a7c5831abfb23",
+    "url": "/fergus-quant/static/media/Roboto-Bold.761d6d50.ttf"
+  },
+  {
+    "revision": "a968ef4137bbc2df4355142b5b623706",
+    "url": "/fergus-quant/static/media/Roboto-Bold.a968ef41.woff2"
+  },
+  {
+    "revision": "e45fec0859bc0bfd4b7cafeb228209e9",
+    "url": "/fergus-quant/static/media/Roboto-Bold.e45fec08.eot"
+  },
+  {
+    "revision": "87c71a09fe1143cbc1df6dd63791e2e3",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensed.87c71a09.eot"
+  },
+  {
+    "revision": "fc99fcb270d37ae104790f3a791a06d5",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensed.fc99fcb2.woff2"
+  },
+  {
+    "revision": "fcc1b057e42adc4f451839a18cf0d642",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensed.fcc1b057.woff"
+  },
+  {
+    "revision": "ffe53a17d79842b61f89db938ca566d6",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensed.ffe53a17.ttf"
+  },
+  {
+    "revision": "55c0f02c2ecdda99dcedeb61474b84e3",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensedItalic.55c0f02c.woff2"
+  },
+  {
+    "revision": "73256d8846d6c3380b2e3482358ab01a",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensedItalic.73256d88.eot"
+  },
+  {
+    "revision": "b7008316e802534a110ed67eeb1515f4",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensedItalic.b7008316.ttf"
+  },
+  {
+    "revision": "dfc0464a2e53f41c429144030b4ead73",
+    "url": "/fergus-quant/static/media/Roboto-BoldCondensedItalic.dfc0464a.woff"
+  },
+  {
+    "revision": "05cdd569945f256b03c862390f05efe2",
+    "url": "/fergus-quant/static/media/Roboto-BoldItalic.05cdd569.ttf"
+  },
+  {
+    "revision": "42fbcdf2fdec6109d81f4664ff660bee",
+    "url": "/fergus-quant/static/media/Roboto-BoldItalic.42fbcdf2.woff2"
+  },
+  {
+    "revision": "6f7aa0e77da3605127e768c114ffc1e3",
+    "url": "/fergus-quant/static/media/Roboto-BoldItalic.6f7aa0e7.eot"
+  },
+  {
+    "revision": "cf51c6f03470d9b902682888dbbfd0d0",
+    "url": "/fergus-quant/static/media/Roboto-BoldItalic.cf51c6f0.woff"
+  },
+  {
+    "revision": "41dffffe73a552793065b2b8ab456ce1",
+    "url": "/fergus-quant/static/media/Roboto-Condensed.41dffffe.woff"
+  },
+  {
+    "revision": "48f2869583cb2ed3db4f95a2e3560f5d",
+    "url": "/fergus-quant/static/media/Roboto-Condensed.48f28695.woff2"
+  },
+  {
+    "revision": "6c6c38a8ba0d901f6aa5cff2ebcffd7b",
+    "url": "/fergus-quant/static/media/Roboto-Condensed.6c6c38a8.eot"
+  },
+  {
+    "revision": "7c9bdf6ffdd1b1c56109aad6ef31bad8",
+    "url": "/fergus-quant/static/media/Roboto-Condensed.7c9bdf6f.ttf"
+  },
+  {
+    "revision": "1815774597750951f95f818c182bd54a",
+    "url": "/fergus-quant/static/media/Roboto-CondensedItalic.18157745.eot"
+  },
+  {
+    "revision": "63d76aaf8b7bb35f545f31df28bff4a9",
+    "url": "/fergus-quant/static/media/Roboto-CondensedItalic.63d76aaf.ttf"
+  },
+  {
+    "revision": "78db419d969a1d78c5d3bcae387781b1",
+    "url": "/fergus-quant/static/media/Roboto-CondensedItalic.78db419d.woff"
+  },
+  {
+    "revision": "b6c6f2b64b981401e64215137aeb536e",
+    "url": "/fergus-quant/static/media/Roboto-CondensedItalic.b6c6f2b6.woff2"
+  },
+  {
+    "revision": "0fff9dc2f67ee9bcacad57ad00f4d8d6",
+    "url": "/fergus-quant/static/media/Roboto-Italic.0fff9dc2.woff2"
+  },
+  {
+    "revision": "21c2751d5ea6f77f6e78aaecf9234ec7",
+    "url": "/fergus-quant/static/media/Roboto-Italic.21c2751d.eot"
+  },
+  {
+    "revision": "4e2a8ee2a2609a9d828807c383b9e950",
+    "url": "/fergus-quant/static/media/Roboto-Italic.4e2a8ee2.woff"
+  },
+  {
+    "revision": "6095a5d1806ef620756346f0b26cbb0a",
+    "url": "/fergus-quant/static/media/Roboto-Italic.6095a5d1.ttf"
+  },
+  {
+    "revision": "520e872565e0df5d77eead93bbd55e62",
+    "url": "/fergus-quant/static/media/Roboto-Light.520e8725.woff"
+  },
+  {
+    "revision": "a0db358aaf4e200508f2496fd4688497",
+    "url": "/fergus-quant/static/media/Roboto-Light.a0db358a.eot"
+  },
+  {
+    "revision": "aa299040aa204bfa981fe1166120c7b1",
+    "url": "/fergus-quant/static/media/Roboto-Light.aa299040.ttf"
+  },
+  {
+    "revision": "e04aee294608887aa75f438ab3b4ea4f",
+    "url": "/fergus-quant/static/media/Roboto-Light.e04aee29.woff2"
+  },
+  {
+    "revision": "01178e9dfc82f833c4d0fd9aeeb85027",
+    "url": "/fergus-quant/static/media/Roboto-LightItalic.01178e9d.ttf"
+  },
+  {
+    "revision": "c62a9aaab9c45e9a4ccb719feb26c538",
+    "url": "/fergus-quant/static/media/Roboto-LightItalic.c62a9aaa.eot"
+  },
+  {
+    "revision": "d1ef74b5828b4104b922594256ec94f0",
+    "url": "/fergus-quant/static/media/Roboto-LightItalic.d1ef74b5.woff2"
+  },
+  {
+    "revision": "df65114e051699c277b6a36203901f43",
+    "url": "/fergus-quant/static/media/Roboto-LightItalic.df65114e.woff"
+  },
+  {
+    "revision": "8a0b44e6574fbeef1308c3f656dd8ea4",
+    "url": "/fergus-quant/static/media/Roboto-Medium.8a0b44e6.eot"
+  },
+  {
+    "revision": "9531d3bf1b81644d28f01bf512505037",
+    "url": "/fergus-quant/static/media/Roboto-Medium.9531d3bf.ttf"
+  },
+  {
+    "revision": "a04a68984ac70a40504e93792e238825",
+    "url": "/fergus-quant/static/media/Roboto-Medium.a04a6898.woff2"
+  },
+  {
+    "revision": "bdb3626e5abcd47e78688afc0bdba672",
+    "url": "/fergus-quant/static/media/Roboto-Medium.bdb3626e.woff"
+  },
+  {
+    "revision": "9a5bda5125436724885c48a23f673a72",
+    "url": "/fergus-quant/static/media/Roboto-MediumItalic.9a5bda51.woff"
+  },
+  {
+    "revision": "b9232ff0cc4f26fc9a1503e84a3cf7e1",
+    "url": "/fergus-quant/static/media/Roboto-MediumItalic.b9232ff0.ttf"
+  },
+  {
+    "revision": "ebbd3d8e98a48030c14331a61a79d1e4",
+    "url": "/fergus-quant/static/media/Roboto-MediumItalic.ebbd3d8e.eot"
+  },
+  {
+    "revision": "f058684730e2e14f4c6a19b05c3bd60b",
+    "url": "/fergus-quant/static/media/Roboto-MediumItalic.f0586847.woff2"
+  },
+  {
+    "revision": "3b5ac9971d8a9298d688c15fe1bb8861",
+    "url": "/fergus-quant/static/media/Roboto-Regular.3b5ac997.eot"
+  },
+  {
+    "revision": "4a96a87f719df92c63282e188b1f942a",
+    "url": "/fergus-quant/static/media/Roboto-Regular.4a96a87f.woff2"
+  },
+  {
+    "revision": "7816c39b8af37e33d6ffc46e7eb93e96",
+    "url": "/fergus-quant/static/media/Roboto-Regular.7816c39b.woff"
+  },
+  {
+    "revision": "b84ef733e3e3bd078cc5c54f1416d21a",
+    "url": "/fergus-quant/static/media/Roboto-Regular.b84ef733.ttf"
+  },
+  {
+    "revision": "2442bca0562e240eeb1daa28c835cd11",
+    "url": "/fergus-quant/static/media/Roboto-Thin.2442bca0.woff2"
+  },
+  {
+    "revision": "75ea60bef32cff0d8277197cb782e055",
+    "url": "/fergus-quant/static/media/Roboto-Thin.75ea60be.woff"
+  },
+  {
+    "revision": "bbb8f417d1a0e667a9dcc224f7813f83",
+    "url": "/fergus-quant/static/media/Roboto-Thin.bbb8f417.ttf"
+  },
+  {
+    "revision": "e93860942347de76a0cab3056e0b8891",
+    "url": "/fergus-quant/static/media/Roboto-Thin.e9386094.eot"
+  },
+  {
+    "revision": "31cceaf8830eee0e74ee06ff453f1fc1",
+    "url": "/fergus-quant/static/media/Roboto-ThinItalic.31cceaf8.eot"
+  },
+  {
+    "revision": "b89869582b8fea445bdacb95a0cbe621",
+    "url": "/fergus-quant/static/media/Roboto-ThinItalic.b8986958.woff2"
+  },
+  {
+    "revision": "f168bf56473914d987921cbcd948c0fa",
+    "url": "/fergus-quant/static/media/Roboto-ThinItalic.f168bf56.woff"
+  },
+  {
+    "revision": "fcd584b53896ec624347d7459cabbff8",
+    "url": "/fergus-quant/static/media/Roboto-ThinItalic.fcd584b5.ttf"
+  },
+  {
+    "revision": "3528d87665424e3cd444628ef0ec4da6",
+    "url": "/fergus-quant/static/media/icomoon.3528d876.eot"
+  },
+  {
+    "revision": "65b8b376c1130f53e058673b1691e0d5",
+    "url": "/fergus-quant/static/media/icomoon.65b8b376.svg"
+  },
+  {
+    "revision": "ebc0aee487fc3b0d63ecbf2cc29dcd1e",
+    "url": "/fergus-quant/static/media/icomoon.ebc0aee4.woff"
+  },
+  {
+    "revision": "fa36e9fd98fca300997ec0db11b3a5f8",
+    "url": "/fergus-quant/static/media/icomoon.fa36e9fd.ttf"
+  },
+  {
+    "revision": "6feb41c385923b109e50d60408883b80",
+    "url": "/fergus-quant/static/media/raccoon.6feb41c3.png"
+  }
+]);
